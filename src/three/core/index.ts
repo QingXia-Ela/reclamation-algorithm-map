80c8c5895979af0bddbdf2f35cc5c6d40d804310
QingXia-Ela <Elahere@163.com>
2024-02-24 12:28:24 +0800

    feat: 补充缺失文字

--- src/three/core/index.ts
+++ src/three/core/index.ts
@@ -252,7 +252,9 @@ class MapCore {
 
     camera.position.set(0, 0, 20);
 
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true
+    });
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     this.threeObject = {

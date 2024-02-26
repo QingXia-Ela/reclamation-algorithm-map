fee5c22353b2bc99f93ea49fd83094c043c7d3ba
LuoTianOrange <2977277015@qq.com>
2024-02-26 23:54:41 +0800

    将节点菜单中节点描述的el-select改为 el-autocomplete

--- src/components/RightNodeInfo/components/components/NoteSelector.vue
+++ src/components/RightNodeInfo/components/components/NoteSelector.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ElInput, ElSelect, ElOption, ElFormItem } from "element-plus"
+import { ElInput, ElSelect, ElOption, ElFormItem, ElAutocomplete } from "element-plus"
 import { ref } from "vue";
 import { NodeDescriptionData } from "@/constants/node";
 
@@ -26,14 +26,29 @@ function onPresetChange(value) {
 function onInputChange() {
   select.value = "自定义"
 }
+
+function querySearch(queryString, cb) {
+  const results = queryString ? options.filter(createFilter(queryString)) : options;
+  cb(results);
+}
+
+function createFilter(queryString) {
+  return (option) => {
+    return (option.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+  };
+}
+
 </script>
 
 <template>
   <el-form-item label="节点描述">
     <!-- todo!: 改为 el-autocomplete -->
-    <el-select v-model="select" placeholder="选择预设..." style="margin-bottom: .4rem;" @change="onPresetChange">
+    <!-- <el-select v-model="select" placeholder="选择预设..." style="margin-bottom: .4rem;" @change="onPresetChange">
       <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
-    </el-select>
+    </el-select> -->
+    <el-autocomplete v-model="select" :fetch-suggestions="querySearch" placeholder="选择预设..." style="margin-bottom: .4rem;"
+      @select="onPresetChange" class="input-with-select">
+    </el-autocomplete>
     <el-input v-model="note" placeholder="输入节点描述" class="input-with-select" type="textarea" />
   </el-form-item>
 </template>

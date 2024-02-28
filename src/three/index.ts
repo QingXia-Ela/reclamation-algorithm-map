8a149ff2967ce89fef2ed13a0539e1c9a29c976b
QingXia-Ela <Elahere@163.com>
2024-02-28 20:43:12 +0800

    fix: 修复生产环境获取旧地图时出错的问题

--- src/three/index.ts
+++ src/three/index.ts
@@ -1,37 +1,13 @@
-import { getDataFromLocal, saveDataToLocal } from '@/utils/three/localStoreMapData'
+import { getDataFromLocal } from '@/utils/three/localStoreMapData'
 import MapCore from './core'
 import type { SaveMapData } from './types/data'
-import { ElMessage } from 'element-plus'
 
 const core = new MapCore()
 
-const data = getDataFromLocal()
+// const data = getDataFromLocal()
 
-async function fetchData() {
-  return await fetch("map.json").then(res => res.json()).then(map => {
-    core.loadData(map as SaveMapData)
-  })
-}
-
-if (process.env.NODE_ENV == "production" && data) {
-  try {
-    core.loadData(data as SaveMapData)
-  } catch (e) {
-    ElMessage({
-      message: "本地地图数据加载失败，将载入远程服务器数据",
-      type: "info",
-    })
-    fetchData().then(() => {
-      saveDataToLocal(core)
-      ElMessage({
-        message: "远程数据加载成功",
-        type: "success",
-      })
-    })
-  }
-}
-else {
-  fetchData()
-}
+fetch("map.json").then(res => res.json()).then(map => {
+  core.loadData(map as SaveMapData)
+})
 
 export default core

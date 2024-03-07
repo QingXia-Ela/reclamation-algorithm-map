68b8737fbca673abaf1315d653aedc4e51371dbe
QingXia-Ela <Elahere@163.com>
2024-03-07 22:42:27 +0800

    feat: 更新坚守阵地地图

--- src/three/object/background/index.ts
+++ src/three/object/background/index.ts
@@ -18,28 +18,56 @@ async function getMapTextureFromType(type: MapType) {
   }
 }
 
+let mainBackground: THREE.Mesh | null = null
+
+async function getMainBackground() {
+  if (!mainBackground) {
+    mainBackground = new THREE.Mesh(
+      new THREE.PlaneGeometry(200, 200),
+      new THREE.MeshBasicMaterial({
+        transparent: true,
+        map: await getMapTextureFromType('main')
+      })
+    )
+  }
+
+  return mainBackground
+}
+
+async function getDungeonBackground(type: MapType) {
+  return new THREE.Mesh(
+    new THREE.PlaneGeometry(80, 80),
+    new THREE.MeshBasicMaterial({
+      transparent: true,
+      map: await getMapTextureFromType(type)
+    })
+  )
+}
+
+async function getBackgroundByType(type: MapType) {
+  switch (type) {
+    case 'main':
+      return getMainBackground()
+
+    default:
+      return getDungeonBackground(type)
+  }
+}
+
 class Background extends THREE.Group {
-  material: THREE.MeshBasicMaterial | null = null
+  backgroundObj: THREE.Mesh | null = null
   constructor() {
     super()
     this._init()
   }
 
   private async _init() {
-    const texture = await getMapTextureFromType('main')
 
-    const material = new THREE.MeshBasicMaterial({
-      transparent: true,
-      map: texture
-    })
-    this.material = material
-
-    const PlaneBackground = new THREE.Mesh(
-      new THREE.PlaneGeometry(200, 200),
-      material
-    )
+    const PlaneBackground = await getMainBackground()
 
     PlaneBackground.position.set(0, 0, 0)
+
+    this.backgroundObj = PlaneBackground
 
     this.add(
       PlaneBackground
@@ -52,8 +80,11 @@ class Background extends THREE.Group {
    * @param type 地图类型
    */
   async changeBackground(type: MapType) {
-    const texture = await getMapTextureFromType(type)
-    this.material!.map = texture
+    this.remove(this.backgroundObj!)
+    this.backgroundObj = await getBackgroundByType(type)
+    this.add(
+      this.backgroundObj
+    )
   }
 }
 

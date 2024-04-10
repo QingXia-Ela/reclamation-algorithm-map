efd1f442d9667339f38202abdc6dc8a1067cfa3f
QingXia-Ela <Elahere@163.com>
2024-04-10 23:05:30 +0800

    fix: 修复跳转失败的问题

--- src/components/UserToolbar/components/BluePrint.vue
+++ src/components/UserToolbar/components/BluePrint.vue
@@ -4,11 +4,15 @@ import SvgIcon from '@/components/SvgIcon.vue';
 import { ElNotification } from 'element-plus';
 import { ref } from 'vue';
 import core from '@/three'
+
+function open(url) {
+  window.open(url)
+}
 </script>
 
 <template>
   <OperateButton popover="前往驻扎地蓝图建设（该功能由生息演算官网实现）" :popover-width="180"
-    @click="window.open('https://ak.hypergryph.com/ra/taleswithinthesand/blueprint')">
+    @click="open('https://ak.hypergryph.com/ra/taleswithinthesand/blueprint')">
     <SvgIcon name="logo" color="#eee" style="width: 1.2rem; height: 1.2rem" />
   </OperateButton>
 </template>
